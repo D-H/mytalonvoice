f258f1fb212a28800abc59deb0d7920c3bd0d6bd
David Henderson <davidh@shift.com>
2019-10-20 19:45:13 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Starter Pack of Talon Modules (for use with [Talon](https://talonvoice.com/))
+
+This collection of modules contains a starter pack of voice commands to be used with Talon, a hands-free input replacement. Modules for eye tracking with the Tobii 4C eye tracker are not included in this pack, but can be found in [Talon Community](https://github.com/dwiel/talon_community). 
+
+Some modules were originally copied from [Talon Community](https://github.com/dwiel/talon_community).
+
+
+# Prerequisites
+- Install Talon
+- Install Dragon (most commands may also work with the free built-in voice engine)
+- Microphone (built in microphone won't produce great results, a good microphone makes a significant difference)
+
+Follow the [Getting Started](https://talonvoice.com/docs/) section of the Talon docs.
+
+# Installing
+Clone this repository into `~/.talon/user`. Any files in this directory will automatically be loaded into Talon. 
+
+# Getting Started
+Check out this blog post [Getting Started with Voice Driven Development](https://whalequench.club/blog/2019/09/03/learning-to-speak-code.html).
+
+# Add Additional Modules
+You will need to supplement this starter pack with additional modules depending on your needs. For example, you may need modules for some applications you use, languages you write, or your preferred text editor. Check out the following repositories for additional modules:
+
+* Largest collection maintained by @dwiel: [Talon Community](https://github.com/dwiel/talon_community) 
+* Mine: [Talon Configs](https://github.com/2shea/talon_configs)
+* Examples written by @aegis: [Talon Examples](https://github.com/talonvoice/examples)
+* @anonfunc's set: [Talon User](https://github.com/anonfunc/talon-user)
---
+++ utils.py
@@ -0,0 +1,192 @@
+from talon.voice import Str, press
+import talon.clip as clip
+from talon import resource
+from .bundle_groups import FILETYPE_SENSITIVE_BUNDLES
+import json
+
+# overrides are used as a last resort to override the output. Some uses:
+# - frequently misheard words
+# - force homophone preference (alternate homophones can be accessed with homophones command)
+
+# To add an override, add the word to override as the key and desired replacement as value in overrides.json
+mapping = json.load(resource.open("overrides.json"))
+
+# used for auto-spacing
+punctuation = set(".,-!?")
+
+
+def remove_dragon_junk(word):
+    return str(word).lstrip("\\").split("\\", 1)[0]
+
+
+def parse_word(word):
+    word = remove_dragon_junk(word)
+    word = mapping.get(word.lower(), word)
+    return word
+
+
+def join_words(words, sep=" "):
+    out = ""
+    for i, word in enumerate(words):
+        if i > 0 and word not in punctuation:
+            out += sep
+        out += word
+    return out
+
+
+def parse_words(m):
+    return list(map(parse_word, m.dgndictation[0]._words))
+
+
+def insert(s):
+    Str(s)(None)
+
+
+def text(m):
+    insert(join_words(parse_words(m)).lower())
+
+
+def sentence_text(m):
+    text = join_words(parse_words(m)).lower()
+    insert(text.capitalize())
+
+
+def word(m):
+    text = extract_word(m)
+    insert(text.lower())
+
+
+def extract_word(m):
+    return join_words(list(map(parse_word, m.dgnwords[0]._words)))
+
+
+# FIX ME
+def surround(by):
+    def func(i, word, last):
+        if i == 0:
+            word = by + word
+        if last:
+            word += by
+        return word
+
+    return func
+
+
+# support for parsing numbers as command postfix
+def numeral_map():
+    numeral_map = dict((str(n), n) for n in range(0, 20))
+    for n in [20, 30, 40, 50, 60, 70, 80, 90]:
+        numeral_map[str(n)] = n
+    numeral_map["oh"] = 0  # synonym for zero
+    return numeral_map
+
+
+def numerals():
+    return " (" + " | ".join(sorted(numeral_map().keys())) + ")+"
+
+
+def optional_numerals():
+    return " (" + " | ".join(sorted(numeral_map().keys())) + ")*"
+
+
+def text_to_number(m):
+    tmp = [str(s).lower() for s in m._words]
+    words = [parse_word(word) for word in tmp]
+
+    result = 0
+    factor = 1
+    for word in reversed(words):
+        if word not in optional_numerals():
+            # we consumed all the numbers and only the command name is left.
+            break
+
+        result = result + factor * int(numeral_map()[word])
+        factor = 10 * factor
+
+    return result
+
+
+number_conversions = {"oh": "0"}  # 'oh' => zero
+for i, w in enumerate(
+    ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+):
+    number_conversions[str(i)] = str(i)
+    number_conversions[w] = str(i)
+    number_conversions["%s\\number" % (w)] = str(i)
+
+
+def parse_words_as_integer(words):
+    # TODO: Once implemented, use number input value rather than manually
+    # parsing number words with this function
+
+    # Ignore any potential non-number words
+    number_words = [w for w in words if str(w) in number_conversions]
+
+    # Somehow, no numbers were detected
+    if len(number_words) == 0:
+        return None
+
+    # Map number words to simple number values
+    number_values = list(map(lambda w: number_conversions[w.word], number_words))
+
+    # Filter out initial zero values
+    normalized_number_values = []
+    non_zero_found = False
+    for n in number_values:
+        if not non_zero_found and n == "0":
+            continue
+        non_zero_found = True
+        normalized_number_values.append(n)
+
+    # If the entire sequence was zeros, return single zero
+    if len(normalized_number_values) == 0:
+        normalized_number_values = ["0"]
+
+    # Create merged number string and convert to int
+    return int("".join(normalized_number_values))
+
+
+def is_in_bundles(bundles):
+    return lambda app, win: any(b in app.bundle for b in bundles)
+
+
+def is_filetype(extensions=()):
+    def matcher(app, win):
+        if is_in_bundles(FILETYPE_SENSITIVE_BUNDLES)(app, win):
+            if any(ext in win.title for ext in extensions):
+                return True
+            else:
+                return False
+        return True
+
+    return matcher
+
+
+def preserve_clipboard(fn):
+    def wrapped_function(*args, **kwargs):
+        old = clip.get()
+        ret = fn(*args, **kwargs)
+        sleep(0.1)
+        clip.set(old)
+        return ret
+
+    return wrapped_function
+
+
+@preserve_clipboard
+def jump_to_target(target):
+    press("cmd-left", wait=2000)
+    press("cmd-shift-right", wait=2000)
+    press("cmd-c", wait=2000)
+    press("left", wait=2000)
+    line = clip.get()
+    print("LINE")
+    print(line)
+    result = line.find(target)
+    if result == -1:
+        return
+    for i in range(0, result):
+        press("right", wait=0)
+    for i in range(0, len(target)):
+        press("shift-right")
+    press("right", wait=0)
